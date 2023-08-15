7a18cf716af4e9978d63384c4a650aa2519d8d0d
Graham <blundell.gp@gmail.com>
2023-08-15 16:39:59 -0700

    final commit

--- rspec-peter-graham/rspec.rb
+++ rspec-peter-graham/rspec.rb
@@ -3,15 +3,38 @@ require 'date'
 class Task
     attr_accessor :title, :description, :status, :due_date
 
-    def initialize (title = 'copies', description = 'make copies', due_date = Date.parse('01/01/2024').to_s)
+    def initialize (title = 'copies', description = 'make copies', status = 'in progress', due_date = Date.parse('01/01/2024').to_s)
         @title = title
         @description = description
-        @status = 'in progress'
+        @status = status
         @due_date = due_date
     end
 
-    def update value
-        @status = value
+    def update 
+        @status = 'completed'
     end
+    def print_status 
+         "#{@title} is #{@status}"
+     end
 
+    
 end
+
+task = Task.new
+p task.print_status
+  task.update
+  p task.print_status
+
+class Tasklist < Task
+    attr_accessor :task_collection
+
+    def initialize 
+        @task_collection = []
+
+    end
+    def add_tasks task
+        @task_collection << task
+    end
+    
+end
+
