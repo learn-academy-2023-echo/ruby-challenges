4254a50bc6a3c160c832a9f14dd167c451636eed
Kiana-Massey <kxm7046@mavs.uta.edu>
2023-08-15 14:16:23 -0700

    first push

---
+++ rspec-kiana-megan/rspec.rb
@@ -0,0 +1,20 @@
+
+
+class Netflix 
+
+    attr_accessor :title, :description, :done, :date
+        def initialize(done = 'in_progress', date)
+        @title = title
+        @description = description
+        @done = done
+        @date = date
+
+    end
+end
+
+# class Date
+#     attr_accessor :due_date
+#     def initialize
+#         @due_date = due_date
+#     end
+# end
