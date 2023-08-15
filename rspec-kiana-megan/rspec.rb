a9e47faf4105ed5bdf1923c672d24eb85ff56d74
megandee <meganderisi@gmail.com>
2023-08-15 18:52:58 -0500

    second half of day challenges

--- rspec-kiana-megan/rspec.rb
+++ rspec-kiana-megan/rspec.rb
@@ -1,20 +1,25 @@
 
 
-class Netflix 
+# class Netflix 
 
-    attr_accessor :title, :description, :done, :date
-        def initialize(done = 'in_progress', date)
-        @title = title
-        @description = description
-        @done = done
-        @date = date
+#     attr_accessor :title, :description, :done, :date
+#         def initialize(done = 'in_progress', date = 2023-8-15)
+#         @title = title
+#         @description = description
+#         @done = done
+#         @date = date
 
-    end
-end
-
-# class Date
-#     attr_accessor :due_date
-#     def initialize
-#         @due_date = due_date
 #     end
 # end
+
+# class Netflix < NetflixNew
+#     def initialize(title, description, done, date)
+# end
+
+class Task
+    attr_accessor :title, :description, :done
+    def initialize(done = 'in_progress')
+    @title = title
+    @description = description
+    end
+end
--- rspec-kiana-megan/rspec_spec.rb
+++ rspec-kiana-megan/rspec_spec.rb
@@ -1,17 +1,10 @@
 # As a developer, I can create a Task.
-
-
 # As a developer, I can give a Task a title and retrieve it.
-
-
 # As a developer, I can give a Task a description and retrieve it.
-
-
 # As a developer, I can mark a Task done. Tasks should be initialized as 'in progress'.
-
-
 # As a developer, when I print a Task that is done, its status is shown.
 # As a developer, I can give a Task a due date. Hint: Use the built-in Ruby Date class.
+
 # 🏔 Stretch Goals
 # As a developer, I can add all of my Tasks to a TaskList.
 # As a developer with a TaskList, I can print the completed items.
@@ -22,39 +15,75 @@
 require 'rspec'
 require_relative 'rspec.rb'
 
-describe 'Netflix' do
+# describe 'Netflix' do
 
-    my_Netflix = Netflix.new
-    it 'new tv_show instantiated' do
-        expect{my_Netflix}.to_not raise_error
+#     my_Netflix = Netflix.new
+#     it 'new tv_show instantiated' do
+#         expect{my_Netflix}.to_not raise_error
+#     end
+
+#     it 'has a title' do
+#         my_Netflix.title = 'Bones'
+#         expect(my_Netflix.title).to be_a String
+#         expect(my_Netflix.title).to eq 'Bones'
+#     end
+
+#     it 'has a description' do
+#         my_Netflix.description = 'a crime comedy'
+#         expect(my_Netflix.description).to be_a String
+#         expect(my_Netflix.description).to eq 'a crime comedy'
+#     end
+
+#     it 'in progress' do
+#         my_Netflix.done = 'in progress'
+#         expect(my_Netflix.done).to be_a String
+#         expect(my_Netflix.done).to eq 'in progress'
+#     end
+
+#     it 'show status' do
+#         my_Netflix.done = 'done'
+#         expect(my_Netflix.done).to be_a String
+#         expect(my_Netflix.done).to eq 'done'
+#     end
+
+#     
+#     it 'date is instantiated' do
+#         my_Netflix.date = 2023-8-15
+#         expect(my_Netflix.date).to be_a Integer
+#         # expect{my_Netflix.date}.to_not raise_error
+#         expect(my_Netflix.date).to eq 2023-8-15
+#     end
+# end
+
+# As a developer, I can create a Task. ✅
+# As a developer, I can give a Task a title and retrieve it. ✅
+# As a developer, I can give a Task a description and retrieve it. ✅
+# As a developer, I can mark a Task done. Tasks should be initialized as 'in progress'.
+# As a developer, when I print a Task that is done, its status is shown.
+# As a developer, I can give a Task a due date. Hint: Use the built-in Ruby Date class.
+
+describe 'Task' do
+    my_task = Task.new 
+    it 'new show instantiated' do
+        expect{ my_task}.to_not raise_error
     end
 
     it 'has a title' do
-        my_Netflix.title = 'Bones'
-        expect(my_Netflix.title).to be_a String
-        expect(my_Netflix.title).to eq 'Bones'
+        my_task.title = 'Clean Kitchen'
+        expect(my_task.title).to be_a String
+        expect(my_task.title).to eq 'Clean Kitchen'
     end
 
     it 'has a description' do
-        my_Netflix.description = 'a crime comedy'
-        expect(my_Netflix.description).to be_a String
-        expect(my_Netflix.description).to eq 'a crime comedy'
-    end
-
-    it 'in progress' do
-        my_Netflix.done = 'in progress'
-        expect(my_Netflix.done).to be_a String
-        expect(my_Netflix.done).to eq 'in progress'
+        my_task.description = 'Sweep'
+        expect(my_task.description).to be_a String
+        expect(my_task.description).to eq 'Sweep'
     end
 
     it 'show status' do
-        my_Netflix.done = 'done'
-        expect(my_Netflix.done).to be_a String
-        expect(my_Netflix.done).to eq 'done'
-    end
-
-    my_Netflix = Date.new
-    it 'date is instantiated' do
-        expect{my_Netflix}.to_not raise_error
+        my_task.done = 'done'
+        expect(my_task.done).to be_a String
+        expect(my_task.done).to eq 'done'
     end
 end
+
